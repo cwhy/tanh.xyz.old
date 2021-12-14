27c2c96334db9ad673fcc0b47cf1d49c573f40f3
Chen Yu <chenyu.nus@gmail.com>
2021-12-14 11:37:16 +0000

    added gitignore

--- drafts/math_from_the_ground_up/rust/kids_math/target/debug/deps/kids_math-5bbc9aab12ac286f.d
+++
@@ -1,5 +0,0 @@
-/home/owner/Projects/tanh.xyz/drafts/math_from_the_ground_up/rust/kids_math/target/debug/deps/kids_math-5bbc9aab12ac286f: src/main.rs
-
-/home/owner/Projects/tanh.xyz/drafts/math_from_the_ground_up/rust/kids_math/target/debug/deps/kids_math-5bbc9aab12ac286f.d: src/main.rs
-
-src/main.rs:
--- drafts/math_from_the_ground_up/rust/kids_math/target/debug/kids_math.d
+++
@@ -1,1 +0,0 @@
-/home/owner/Projects/tanh.xyz/drafts/math_from_the_ground_up/rust/kids_math/target/debug/kids_math: /home/owner/Projects/tanh.xyz/drafts/math_from_the_ground_up/rust/kids_math/src/main.rs
